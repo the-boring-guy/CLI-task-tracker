5aa4fec57d52bd6ba5d7462b09d96a77cf4d7610
the-boring-guy <professionalmail.laksh@gmail.com>
2026-01-09 16:24:42 +0530

    Created task class to create new tasks

---
+++ task.py
@@ -0,0 +1,16 @@
+class Task:
+    def __init__(self, id, description, status, created_at, last_modified_at):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.created_at = created_at
+        self.last_modified_at = last_modified_at
+
+    def mark_not_done():
+        pass
+
+    def mark_done():
+        pass
+
+    def mark_in_transit():
+        pass
