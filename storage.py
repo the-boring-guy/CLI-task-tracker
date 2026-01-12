5e0f6ff4cf2838e22d5ec244d51fdb0c2ccffc73
the-boring-guy <professionalmail.laksh@gmail.com>
2026-01-12 18:21:49 +0530

    First real progress in handling storage.json

---
+++ storage.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+
+STORAGE_FILE = "storage.json"
+
+
+def load_tasks():
+    try:
+        with open(STORAGE_FILE, 'r') as file:
+            content = json.load(file)
+            return content
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
+
+
+def add_task():
+    pass
+
+
+def remove_task():
+    pass
+
+
+def save_tasks():
+    pass
