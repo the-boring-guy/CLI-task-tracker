766ff460fc83c65a056a60ffde5f59f7af789a6b
the-boring-guy <professionalmail.laksh@gmail.com>
2026-01-14 19:55:16 +0530

    Wrote code for add task and also fixed minor error in utils

--- storage.py
+++ storage.py
@@ -21,8 +21,23 @@ def save_tasks(task_list):
         json.dump(task_list, file)
 
 
-def add_task(task):
-    pass
+def get_task_id():
+    task_list = load_tasks()
+    if task_list == []:
+        return 1
+    else:
+        pass
+
+
+def add_task(task_name, task_description):
+    task_list = load_tasks()
+    task_created_at = utils.get_current_time()
+    task_id = get_task_id()
+    task_status = "NOT_STARTED"
+    task = {"id": task_id, "name": task_name, "description": task_description,
+            "status": task_status, "created_at": task_created_at, "last_modified_at": task_created_at}
+    task_list.append(task)
+    save_tasks(task_list)
 
 
 def remove_task(task):
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import datetime
-import json
 
 
 def get_current_time():
     now = datetime.datetime.now()
     now = now.strftime("%H:%M:%S on %d-%m-%Y")
+    return now
