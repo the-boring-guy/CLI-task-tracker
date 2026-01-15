796552cca7c469e78c11f625cf7d9f20f9916608
the-boring-guy <professionalmail.laksh@gmail.com>
2026-01-15 11:36:31 +0530

    add, load and save tasks is done

--- storage.py
+++ storage.py
@@ -8,37 +8,34 @@ STORAGE_FILE = "storage.json"
 def load_tasks():
     try:
         with open(STORAGE_FILE, 'r') as file:
-            task_list = json.load(file)
-            return task_list
+            task_dictionary = json.load(file)
+            return dict(task_dictionary)
     except FileNotFoundError:
-        return []
+        return {"last_id": 0, "tasks": []}
     except json.JSONDecodeError:
-        return []
+        return {"last_id": 0, "tasks": []}
 
 
-def save_tasks(task_list):
+def save_tasks(task_dictionary):
     with open(STORAGE_FILE, 'w') as file:
-        json.dump(task_list, file)
-
-
-def get_task_id():
-    task_list = load_tasks()
-    if task_list == []:
-        return 1
-    else:
-        pass
+        json.dump(task_dictionary, file)
 
 
 def add_task(task_name, task_description):
-    task_list = load_tasks()
+    task_dictionary = load_tasks()
+    task_list = list(task_dictionary.get("tasks"))
+    last_id = task_dictionary.get("last_id")
     task_created_at = utils.get_current_time()
-    task_id = get_task_id()
+    task_id = last_id + 1
     task_status = "NOT_STARTED"
     task = {"id": task_id, "name": task_name, "description": task_description,
             "status": task_status, "created_at": task_created_at, "last_modified_at": task_created_at}
     task_list.append(task)
-    save_tasks(task_list)
+    task_dictionary.update({"tasks": task_list})
+    last_id += 1
+    task_dictionary.update({"last_id": last_id})
+    save_tasks(task_dictionary)
 
 
-def remove_task(task):
+def remove_task(task_id):
     pass
