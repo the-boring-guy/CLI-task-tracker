51a38efc6dad43a3d1419b2aa9f804e790fca692
the-boring-guy <professionalmail.laksh@gmail.com>
2026-01-19 19:56:18 +0530

    major bug fix in get_task_id due to different data types

--- main.py
+++ main.py
@@ -42,8 +42,9 @@ def get_task_id():
         elif utils.check_natural_number(task_id) is False:
             print("INVALID INPUT!!!! TASK ID MUST BE A NATURAL NUMBER....")
         else:
+            task_id = int(task_id)
             if storage.validate_task_id(task_id):
-                return int(task_id)
+                return (task_id)
             else:
                 print("INVALID INPUT!!!! TASK LINKED TO THIS ID DOES NOT EXIST....")
 
